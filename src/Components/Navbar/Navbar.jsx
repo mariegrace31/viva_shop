15f4c792567a0b05564a99fef8a4c3f04e132ef0
mariegrace31 <mariegracebmg@gmail.com>
2024-01-09 15:41:06 +0200

    Style the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -11,14 +13,15 @@ const Navbar = () => {
         <p>VIVA SHOP</p>
       </div>
       <ul>
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => {setMenu("shop")}}>Shop {menu==="shop"?<hr />:<></>}</li>
+        <li onClick={() => {setMenu("men")}}>Men {menu==="men"?<hr />:<></>}</li>
+        <li onClick={() => {setMenu("women")}}>Women {menu==="women"?<hr />:<></>}</li>
+        <li onClick={() => {setMenu("kids")}}>Kids {menu==="kids"?<hr />:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
