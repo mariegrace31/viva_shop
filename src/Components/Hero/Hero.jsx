5a2e1c2c67e44d3f3b8cb7cdc89a0b5fae36cc41
mariegrace31 <mariegracebmg@gmail.com>
2024-01-10 22:52:20 +0200

    Add content in the hero file

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Hero.css';
+import hand_icon from '../Assets/hand_icon.png';
+import arrow_icon from '../Assets/arrow.png';
+import hero_image from '../Assets/hero_image.png';
 
 const Hero = () => {
   return (
     <div className='hero'>
-      Hero
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hand-hand-icon">
+            <p>New</p>
+            <img src={hand_icon} alt="hand-icon" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <p>Latest Collection</p>
+          <img src={arrow_icon} alt="arrow-icon" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="hero-image" />
+      </div>
     </div>
   )
 }
