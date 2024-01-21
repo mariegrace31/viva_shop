9670e5e603af5daab69170f2863c53985239be40
mariegrace31 <mariegracebmg@gmail.com>
2024-01-21 22:34:41 +0200

    Add popular items

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Popular.css';
+import data_product from '../Assets/data';
+
+const Popular = () => {
+  return (
+    <div className='popular'>
+      <h1>POPLAR IN WOMEN</h1>
+      <hr />
+    </div>
+  )
+}
+
+export default Popular;
